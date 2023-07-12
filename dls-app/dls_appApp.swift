ddc565c5324674ea4c8bbd535adfd79f81abb435
Griselda Morales <griselda.juarez@frogdesign.com>
2023-07-11 20:36:18 -0600

    Initial Commit

---
+++ dls-app/dls_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dls_appApp.swift
+//  dls-app
+//
+//  Created by Griselda Morales on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct dls_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
